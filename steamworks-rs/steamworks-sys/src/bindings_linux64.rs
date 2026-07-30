095ed3d1b12d9e82668dc1e8c326bb2e8aa1a137
ultimate-yuri <ultimate_yuri@outlook.com>
2026-07-31 01:16:28 +0300

    Add files via upload

--- steamworks-rs/steamworks-sys/src/bindings_linux64.rs
+++ steamworks-rs/steamworks-sys/src/bindings_linux64.rs
@@ -28633,7 +28633,7 @@ extern "C" {
 	pub fn SteamAPI_SteamUGC_v017() -> *mut ISteamUGC;
 }
 extern "C" {
-	pub fn SteamAPI_SteamGameServerUGC_v017() -> *mut ISteamUGC;
+	pub fn SteamAPI_SteamGameServerUGC_v021() -> *mut ISteamUGC;
 }
 extern "C" {
 	pub fn SteamAPI_ISteamUGC_CreateQueryUserUGCRequest(
